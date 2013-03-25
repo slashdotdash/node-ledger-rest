f36260ddc92a570639f81dc2c67ad8c698a6dde1
Ben Smith <ben@slashdotdash.net>
2013-03-25 19:19:05 +0000

    Created spec for balance endpoint using JSON client.

---
+++ spec/balance.spec.js
@@ -0,0 +1,80 @@
+var restify = require('restify'),
+    LedgerRest = require('../lib/ledger-rest').LedgerRest;
+
+describe('Balance', function() {
+  var spec, server, client;
+  
+  // start ledger-rest server
+  function startServer() {
+    server = new LedgerRest({ file: 'spec/data/single-transaction.dat' });
+    server.listen(3000);
+  }
+  
+  function stopServer(done) {
+    server.close(done);
+  }
+  
+  // create JSON client
+  function createClient() {
+    client = restify.createJsonClient({
+      url: 'http://localhost:3000',
+      version: '*',
+      headers: {
+        connection: 'close'
+      }
+    });
+  }
+  
+  beforeEach(function() {
+    spec = this;
+    
+    startServer();
+    createClient();    
+  });
+
+  afterEach(function(done) {
+    stopServer(done);
+  });
+  
+  describe('single transaction', function() {
+    var balances;
+    
+    beforeEach(function(done) {
+      client.get('/balance', function(err, req, res, obj) {
+        if (err) {
+          spec.fail(err);
+          return done();
+        }
+
+        balances = obj;
+        done();
+      });
+    });
+
+    it("should return balance for two accounts", function() {
+      expect(balances.length).toBe(2);
+    });
+
+    it("should parse first balance", function() {
+      expect(balances[0]).toEqual({ 
+        total: '£1,000.00', 
+        account: { 
+          fullname: 'Assets:Checking',
+          shortname: 'Assets:Checking',
+          depth: 2,
+        }
+      });
+    });
+    
+    it("should parse second balance", function() {
+      expect(balances[1]).toEqual({ 
+        total: '£-1,000.00', 
+        account: { 
+          fullname: 'Income:Salary', 
+          shortname: 'Income:Salary',
+          depth: 2,
+        } 
+      });
+    });
+  });
+});
